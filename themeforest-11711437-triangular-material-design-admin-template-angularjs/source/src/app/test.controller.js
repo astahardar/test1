3fe30ac9d35eadf57efbd2cf815760d9ee449484
Pétur V. Guðbergsson <petur@gagarin.is>
2016-08-14 16:00:15 +0000

    Pjetr

--- themeforest-11711437-triangular-material-design-admin-template-angularjs/source/src/app/test.controller.js
+++ themeforest-11711437-triangular-material-design-admin-template-angularjs/source/src/app/test.controller.js
@@ -6,8 +6,14 @@
         .controller('TestController', TestController);
 
     /* @ngInject */
-    function TestController() {
+    function TestController($log) {
         var vm = this;
+
+        vm.WordIndex = 0;
+        vm.Answer = Answer;
+        vm.AnswerData = [];
+        vm.IsDone = false;
+
         vm.Test = {
             Title : 'Title',
             ParsedText :{
@@ -28,5 +34,22 @@
                 }
             }
         };
+
+        vm.Words = vm.Test.ParsedText.Sentence.WORDS;
+
+        function Answer(wordIndex, answerID)
+        {
+          if(!vm.IsDone)
+          {
+              vm.AnswerData[wordIndex] = answerID;
+              $log.log(vm.Words[wordIndex].Word + " answered with : " + answerID);
+              vm.WordIndex++;
+
+              if(vm.WordIndex > vm.Words.length-1)
+              {
+                  vm.IsDone = true;
+              }
+          }
+        }
     }
 })();
